81b6333c4baca50e17ef8109a3a83db3973f001a
pattonsgirl <kayleigh.duncan@wright.edu>
2021-09-26 15:03:37 +0000

    blackened and sorted

--- Discord-Bot-Demo/bot.py
+++ Discord-Bot-Demo/bot.py
@@ -1,4 +1,4 @@
-#libraries needed: discord.py & python-dotenv
+# libraries needed: discord.py & python-dotenv
 import os
 
 import discord
@@ -6,24 +6,26 @@ import random
 from dotenv import load_dotenv
 
 load_dotenv()
-#print(os.getenv('DISCORD_TOKEN'))
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+# print(os.getenv('DISCORD_TOKEN'))
+TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv("DISCORD_GUILD")
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f"{client.user} has connected to Discord!")
 
     for guild in client.guilds:
         if guild.name == GUILD:
             break
 
     print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
+        f"{client.user} is connected to the following guild:\n"
+        f"{guild.name}(id: {guild.id})"
     )
+
 
 @client.event
 async def on_message(message):
@@ -31,25 +33,26 @@ async def on_message(message):
         return
 
     brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
+        "I'm the human form of the 💯 emoji.",
+        "Bingpot!",
         (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
+            "Cool. Cool cool cool cool cool cool cool, "
+            "no doubt no doubt no doubt no doubt."
         ),
     ]
 
     hitchhiker_quotes = [
-        'There is an art, it says, or rather, a knack to flying. The knack lies in learning how to throw yourself at the ground and miss.',
-        'It is a mistake to think you can solve any major problems just with potatoes.',
-        'In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.',
-        'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.',
-        'This is my new quote',
-        ]
+        "There is an art, it says, or rather, a knack to flying. The knack lies in learning how to throw yourself at the ground and miss.",
+        "It is a mistake to think you can solve any major problems just with potatoes.",
+        "In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.",
+        "A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.",
+        "This is my new quote",
+    ]
 
-    if message.content == 'towel!':
-        #response = random.choice(brooklyn_99_quotes)
+    if message.content == "towel!":
+        # response = random.choice(brooklyn_99_quotes)
         response = random.choice(hitchhiker_quotes)
         await message.channel.send(response)
+
 
 client.run(TOKEN)
